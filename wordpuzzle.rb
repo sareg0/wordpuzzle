5cd0a065bee578555042de77e354d9f095600c1b
Sara Regan <regan.sara@gmail.com>
2016-09-06 19:31:20 +0200

    manages to sort up to three letter words

--- wordpuzzle.rb
+++ wordpuzzle.rb
@@ -10,7 +10,7 @@
 #     expect(anagram_finder(['i','t'])).to contain_exactly('it', 'ti')
 #   end
 
-#   it 'combine known letters with additional letter' do
+#   it 'combines known letters with additional letter' do
 #     expect(word_maker('i')).to include(["i", "b"], ["i", "c"], ["i", "d"])
 #   end
 
@@ -34,38 +34,54 @@
 ## take all words with letter 'i'
 
 WORDS = File.readlines('/usr/share/dict/words').map { |w| w.downcase.chomp }
-FILTERED_WORDS = WORDS.select { |word| word.size <= 9 && word.include?('i') }
+FILTERED_LIST = WORDS.select { |word| word.size <= 9 && word.include?('i')}
+
+# puts FILTERED_LIST.length
+#59829
+
 ALPHABET = ('a'..'z').to_a
-puts WORDS.length
 
-two_letter_words = FILTERED_WORDS.select { |word| word.size == 2 }
-characters = ['i']
-two_letter_words.select do |word|
-  letter_array = word.split("")
-  characters << letter_array
-  characters = characters.flatten.uniq
-end
-
-print characters
-puts
-puts two_letter_words.length
-
-# def anagram_finder(letters)
-#   filtered_words = WORDS.select { |w| letters.size == w.chomp.size }
-#   letters.each do |l|
-#     filtered_words = filtered_words.select { |w| w.include?(l) }
-#   end
-#   return filtered_words
-#   puts filtered_words
+# characters = ['i']
+# two_letter_words.select do |word|
+#   letter_array = word.split("")
+#   characters << letter_array
+#   characters = characters.flatten.uniq
 # end
+
+def anagram_finder(current_word, word_length=2)
+  filtered_words = FILTERED_LIST.select do |word|
+    word.length == word_length && (word.chars & current_word.chars).length == current_word.length
+  end.uniq
+  p filtered_words
+
+  # letters.each do |l|
+  #   l = l.is_a?(String) ? l.chars : l
+  #   counter_letter_words.select do |word|
+  #     l.all?{ |c| word.chars.include?(c) }
+  #     letters << word
+  #     letters.uniq!
+  #   end
+  # end
+  # p letters
+
+  # if word_length > 2
+  #   p "end of script"
+  #   return
+  # end
+  return if word_length >= 9
+  #memoise
+
+  filtered_words.each do |word|
+    anagram_finder(word, word_length+1)
+  end
+end
 
 # def word_maker(letters)
 #   letter_combinations = []
 #   ALPHABET.each do |a|
-#     letter_combinations.push([letters.to_s, a.to_s])
+#     letter_combinations = letter_combinations.push([letters.to_s, a.to_s])
 #   end
 #   return letter_combinations
-#   puts letter_combinations
 # end
 
 # word_combis = []
@@ -73,74 +89,5 @@ puts two_letter_words.length
 #   word_combis = word_combis.push(anagram_finder(a))
 # end
 
-# puts word_combis
 
-# wörter mit 3 buchstaben
-
-
-
-# RETURNS ALL ANAGRAMS
-# def anagram_finder(letters)
-#   filtered_words = WORDS.select { |w| letters.size == w.chomp.size }
-#   letters.each do |l|
-#     filtered_words = filtered_words.select { |w| w.include?(l) }
-#   end
-#   return filtered_words
-# end
-
-# the letter(s) that you have + every letter from the alphabet
-#put them in the anagram finder
-#keep those that are left
-
-#finds 3 letter anagrams
-# def anagram_finder(letters)
-#   filtered_words = WORDS.select { |w| letters.size == w.chomp.size }
-#   filtered_words = filtered_words.select { |w| w.include?(letters[0]) }
-#   filtered_words = filtered_words.select { |w| w.include?(letters[1]) }
-#   filtered_words = filtered_words.select { |w| w.include?(letters[2]) }
-# end
-
-# #get the nine letter words that contain 'i'
-# def find_nine_letter_words(words, word_length, clue)
-#   words.select { |w| w.chomp.length == word_length && w.downcase.include?(clue) }
-# end
-
-
-# puts find_nine_letter_words(words, 9, "i").first
-# puts find_matching_eight_letter_words(words, 8, "i")
-
-
-# #pass in each word you want to compare
-# def compare_word(original, comparator)
-#   puts original
-#   original.each do |word|
-#     word.split("")
-#     word.each do |letter|
-#       #remove letter from 9lw
-#       original = original.delete(letter)
-#       puts original
-#       #compare to 8lws
-#     end
-#   end
-#   puts comparator
-# end
-
-# #compare to 8lw
-
-# # e.g. zygotaxis
-# # remove 'z'
-# # compare 'ygotaxis' with 8 letter words
-# # add the ones found that match to collection
-# # next try 'zgotaxis'
-# # etc.
-
-# #all nine letter words: Take one letter away. Compare with all 8 letter words. Find all 8 letter wordsthat match 9lw - 1
-
-# def find_matching_eight_letter_words(words, word_length, clue)
-#   words.select { |w| w.chomp.length == word_length && w.downcase.include?(clue) }
-# end
-
-# compare_word find_nine_letter_words(words, 9, "i").first(2), find_matching_eight_letter_words(words, 8, "i").first
-
-# # 9lw-1
-# #slice the word
+anagram_finder('i')
